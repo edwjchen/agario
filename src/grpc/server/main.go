beb4c86aa14b0ddbb90b4f2d78e8d89692d4188b
Edward Chen <edwjchen@gmail.com>
2020-05-19 17:09:31 -0700

    adding in new server code for blob

--- src/grpc/server/main.go
+++ src/grpc/server/main.go
@@ -1,18 +1,18 @@
 package main
 import (
-  "encoding/json"
-  "grpc/server/countries"
-  "io/ioutil"
+  // "encoding/json"
+  "grpc/server/blob"
+  // "io/ioutil"
   "log"
   "net"
-  "net/http"
+  // "net/http"
   "golang.org/x/net/context"
   "google.golang.org/grpc"
 )
 func main() {
   grpcServer := grpc.NewServer()
   var server Server
-  countries.RegisterCountryServer(grpcServer, server)
+  blob.RegisterBlobServer(grpcServer, server)
   listen, err := net.Listen("tcp", "0.0.0.0:3000")
   if err != nil {
     log.Fatalf("could not listen to 0.0.0.0:3000 %v", err)
@@ -23,19 +23,19 @@ func main() {
 // Server is implementation proto interface
 type Server struct{}
 // Search function responsible to get the Country information
-func (Server) Search(ctx context.Context, request *countries.CountryRequest) (*countries.CountryResponse, error) {
-  resp, err := http.Get("https://restcountries.eu/rest/v2/name/ " + request.Name)
-  if err != nil {
-    return nil, err
-  }
-  defer resp.Body.Close()
-  jsonData, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-    return nil, err
-  }
-  var data []countries.CountryResponse
-  if err := json.Unmarshal(jsonData, &data); err != nil {
-    return nil, err
-  }
-  return &data[0], nil
+func (Server) Move(ctx context.Context, request *blob.BlobRequest) (*blob.BlobResponse, error) {
+  //for now just echo response with increment on position
+  x := request.Position.GetX()
+  y := request.Position.GetY()
+  x++
+  y++
+  newPos := blob.Position{X: x, Y: y}
+  response := blob.BlobResponse{
+    Position: &newPos,
+    Alive: true,
+    Mass: 0,
+    Players: make([]byte, 0),
+    Food: make([]byte, 0)}
+
+  return &response, nil
 }
