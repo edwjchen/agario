8818d1c4124b17a4eb95a6aec1bf6a203278101c
Edward Chen <edwjchen@gmail.com>
2020-05-24 17:55:16 -0700

    entry server

--- src/peer_to_peer/entryserver.go
+++ src/peer_to_peer/entryserver.go
@@ -4,7 +4,19 @@ import (
 	"google.golang.org/grpc"
 	"net"
 	"log"
+	"sync"
+	"golang.org/x/net/context"
+	"peer_to_peer/entryserver"
+	"peer_to_peer/info"
+	"math"
 )
+
+const JOIN_ACTION = "JOIN"
+const CREATE_ACTION = "CREATE"
+
+const MIN_PLAYERS = 10
+const MAX_PLAYERS = 10000
+const MAP_LENGTH = int32(math.Sqrt(MAX_PLAYERS))
 
 func main() {
 	grpcServer := grpc.NewServer()
@@ -16,16 +28,39 @@ func main() {
   	log.Fatal(grpcServer.Serve(listen))
 }
 
-type EntryServer struct {}
+entryInfo := EntryInfo{MinPlayers: MIN_PLAYERS, MaxPlayers: MAX_PLAYERS, CurrNodes: make([]string, 0)}
 
-func (EntryServer) Init(ctx context.Context, request *blob.InitRequest) (*blob.InitResponse, error) {
-	newBlobId, startX, startY := blobsInfo.NewBlob()
-	log.Println(newBlobId, "has joined")
-	response := blob.InitResponse{
-		Id:   newBlobId,
-		X:    startX,
-		Y:    startY,
-		Mass: STARTING_MASS,
+func (EntryServer) CanStart(ctx context.Context, request *entryserver.CanStartRequest) (*entryserver.CanStartReply, error) {
+	/*
+	if 
+	*/
+	response := &entryserver.CanStartReply {
+		CanStart: entryInfo.CanStart()
 	}
-	return &response, nil
+
+	return respnse, nil
+	
 }
+
+func (EntryServer) Join(ctx context.Context, request *entryserver.JoinRequest) (*entryserver.JoinReply, error) {
+	//get number of players	
+	//if current number of players < MAX-players, return true
+	//get current number of players 
+	//if == 0 
+	//else 
+	joinIp := request.GetIp()
+	var action string
+	var ip string
+	if entryInfo.ShouldCreate() {
+		action = JOIN_ACTION
+		ip = //get IP CALL HERE
+	} else {
+		action = CREATE_ACTION
+		ip = joinIp
+	}
+	response := entryserver.JoinReply{
+		Action: action,
+		Ip: i,
+		MapLength: MAP_LENGTH,
+	}
+} 
---
+++ src/peer_to_peer/info/entry_info.go
@@ -0,0 +1,37 @@
+package info
+
+type EntryInfo struct {
+	mux        sync.Mutex
+	MinPlayers int
+	MaxPlayers int 
+	CurrNodes  []string
+}
+
+func (e *EntryInfo) AddNode(node string) {
+	e.mux.Lock()
+	defer e.mux.Unlock()
+
+	e.CurrNodes = append(e.CurrNodes, node)
+}
+
+func (e *EntryInfo) CanStart() bool {
+	e.mux.Lock()
+	defer e.mux.Unlock()
+
+	return len(e.CurrNodes) >= e.MinPlayers
+}
+
+func (e *EntryInfo) GetNodeIP() string {
+	e.mux.Lock()
+	defer e.mux.Unlock()
+
+	rand.Seed(time.Now().UnixNano())
+	return e.CurrNodes[rand.Intn(len(e.CurrNodes))]
+}
+
+func (e *EntryInfo) ShouldCreate() bool {
+	e.mux.Lock()
+	defer e.mux.Unlock()
+	
+	return len(e.CurrNodes) == 0
+}
