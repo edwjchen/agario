b3d34b76fe1b57959642574d15a6fbe3932c316d
gypang <gypang24@gmail.com>
2020-06-11 17:46:08 -0700

    setup scripts

--- src/setup/aws_setup.py
+++ src/setup/aws_setup.py
@@ -1,8 +1,9 @@
 import boto3
 import os
+import time
 import argparse
 import pprint 
-
+import multiprocessing
 import paramiko
 
 parser = argparse.ArgumentParser(description="This program autocreates, starts, and stop Amazon EC2 Instancess.")
@@ -12,6 +13,7 @@ parser.add_argument('-stop', default=0, help="Number of new instances to stop")
 parser.add_argument('-terminate', default=0, help="Number of new instances to terminate")
 parser.add_argument('-stats', default=False, help="Number of new instances to terminate")
 parser.add_argument('-run', default=True, help="Command to run on running instances")
+parser.add_argument('-setup', default=True, help="Setup instances")
 
 ec2_resource = boto3.resource('ec2',
     aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
@@ -58,7 +60,7 @@ def stop_instances(num):
 def terminate_instances(num):
     instances = list(ec2_resource.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running', 'stopped']}]))
     count = 0
-    num = min(num, len(running_instances))
+    num = min(num, len(instances))
     for i in range(num):
         instance = instances[i]
         count += 1
@@ -76,17 +78,57 @@ def get_stats():
     pp.pprint(stats)
 
     
+def worker(ip):
+    key = paramiko.RSAKey.from_private_key_file("the-key-to-her-heart.pem")
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    client.connect(hostname=ip, username="ubuntu", pkey=key)
+    stdin, stdout, stderr = client.exec_command('rm -rf agario > /dev/null; git clone https://github.com/edwjchen/agario.git; export GOPATH=/home/ubuntu/agario; export PATH=$PATH:/usr/local/go/bin:/home/ubuntu/.local/bin; source .bashrc; bash agario/src/setup/setup.sh')
+    stdin.flush()
+
+    if stdout.channel.recv_exit_status():
+        print(ip, " failed clone")
+
+    # stdin, stdout, stderr = client.exec_command('')
+    # stdin.flush()
+    # for line in stdout.read():
+    #     print(line)
+    # if stdout.channel.recv_exit_status():
+    #     print(ip, " failed setup")
+    
+    client.close()
+
 def setup_instances(num):
-    running_instances = ec2_resource.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+    running_instances = list(ec2_resource.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}]))
     if len(running_instances) < num:
         print("Not all instances are up yet!")
         return
-    key = paramiko.RSAKey.from_private_key_file("~")
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    
-    
 
+    running_instance_ips = [instance.public_dns_name for instance in running_instances]
+    pool = multiprocessing.Pool(len(running_instance_ips))
+    pool.map(worker, running_instance_ips)
+    
+    # for instance in running_instances:
+    #     client = paramiko.SSHClient()
+    #     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    #     client.connect(hostname=instance.public_dns_name, username="ubuntu", pkey=key)
+    #     stdin, stdout, stderr = client.exec_command('git clone https://github.com/edwjchen/agario.git')
+    #     stdin.flush()
+    #     if stdout.channel.recv_exit_status():
+    #         print(instance.public_ip_address, " failed clone")
+
+    #     stdin, stdout, stderr = client.exec_command('cd agario/src/setup')
+    #     stdin.flush()
+    #     if stdout.channel.recv_exit_status():
+    #         print(instance.public_ip_address, " failed cd?")
+
+    #     stdin, stdout, stderr = client.exec_command('. setup.sh')
+    #     stdin.flush()
+    #     if stdout.channel.recv_exit_status():
+    #         print(instance.public_ip_address, " failed setup?")
+        
     pass
     
 # instance_ids = []
@@ -120,6 +162,9 @@ if __name__ == '__main__':
 
     if int(args.terminate):
         terminate_instances(int(args.terminate))
+
+    if int(args.setup):
+        setup_instances(int(args.setup))
 
     if bool(args.stats):
         get_stats()
