16ac57d275aa2a13b597f3b320a256659f116cbb
gypang <gypang24@gmail.com>
2020-06-12 11:09:43 -0700

    Can start and kill entryserver now

--- src/setup/aws_setup.py
+++ src/setup/aws_setup.py
@@ -2,9 +2,10 @@ import boto3
 import os
 import time
 import argparse
-import pprint 
+import pprint
 import multiprocessing
 import paramiko
+import sys
 
 parser = argparse.ArgumentParser(description="This program autocreates, starts, and stop Amazon EC2 Instancess.")
 parser.add_argument('-create', default=0, help="Number of new instances to create")
@@ -17,21 +18,21 @@ parser.add_argument('-setup', default=False, help="Setup instances")
 parser.add_argument('-hostname', default=False, help="Print all hostnames of running instances")
 parser.add_argument('-verify', default=False, help="verify setup")
 
+ENTRY_NAME = ""
+EXPERIMENT_NAME= ""
+
+SERVERS_RUNNING = [] # list of dns names
+
 ec2_resource = boto3.resource('ec2',
     aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
     aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
     region_name='us-west-1')
 
-ec2_client = boto3.client('ec2',
-    aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
-    region_name='us-west-1')
-
 def create_instances(num):
-    instance = ec2_resource.create_instances(ImageId='ami-0318e6f2445586bd7', 
-        InstanceType='t2.micro', 
-        KeyName='the-key-to-her-heart', 
-        MinCount=1, 
+    instance = ec2_resource.create_instances(ImageId='ami-0318e6f2445586bd7',
+        InstanceType='t2.micro',
+        KeyName='the-key-to-her-heart',
+        MinCount=1,
         MaxCount=num,
         Monitoring={'Enabled': False},
         SecurityGroups=['agario']
@@ -54,6 +55,7 @@ def stop_instances(num):
     num = min(num, len(running_instances))
     for i in range(num):
         instance = running_instances[i]
+        print('Stopping ', instance.public_dns_name)
         ec2_resource.instances.filter(InstanceIds=[instance.id]).stop()
         count += 1
     print("Stopped {} instances".format(count))
@@ -83,7 +85,7 @@ def get_hostnames():
     pp = pprint.PrettyPrinter(indent=4)
     pp.pprint([instance.public_dns_name for instance in ec2_resource.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])])
 
-    
+
 def worker(ip):
     key = paramiko.RSAKey.from_private_key_file("the-key-to-her-heart.pem")
     client = paramiko.SSHClient()
@@ -100,7 +102,7 @@ def worker(ip):
     # stdin.flush()
     # if stdout.channel.recv_exit_status():
     #     print(ip, " failed setup")
-    
+
     client.close()
 
 def verify_instance_setup():
@@ -121,8 +123,6 @@ def verify_instance_setup():
     for idx, i in enumerate(running_instance_name):
         verify(idx, i)
 
-
-
 def setup_instances(num):
     running_instances = list(ec2_resource.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}]))
     if len(running_instances) < num:
@@ -132,29 +132,89 @@ def setup_instances(num):
     running_instance_ips = [instance.public_dns_name for instance in running_instances]
     pool = multiprocessing.Pool(len(running_instance_ips))
     pool.map(worker, running_instance_ips)
+
+def refresh(name):
+    print("Refreshing ", name)
+    key = paramiko.RSAKey.from_private_key_file("the-key-to-her-heart.pem")
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    client.connect(hostname=name, username="ubuntu", pkey=key)
+    stdin, stdout, stderr = client.exec_command('cd agario; git pull')
+    stdin.flush()
+
+    if stdout.channel.recv_exit_status():
+        print(ip, " failed clone")
+    client.close()
+
+
+def refresh_instances():
+    running_instances = list(ec2_resource.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}]))
+    running_instance_ips = [instance.public_dns_name for instance in running_instances]
+    pool = multiprocessing.Pool(len(running_instance_ips))
+    pool.map(refresh, running_instance_ips)
+
+def start_entry():
+    global ENTRY_NAME
+    running_instances = list(ec2_resource.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}]))
+    if not len(running_instances):
+        print("!!Error!!: No instances currently running!")
+
+    entry_instance = running_instances[0]
+    ENTRY_NAME = entry_instance.public_dns_name
+
+    key = paramiko.RSAKey.from_private_key_file("the-key-to-her-heart.pem")
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    print("Starting entryserver on", ENTRY_NAME)
+    client.connect(hostname=ENTRY_NAME, username="ubuntu", pkey=key)
+    stdin, stdout, stderr = client.exec_command('export GOPATH=/home/ubuntu/agario; /usr/local/go/bin/go run agario/src/peer_to_peer/entryserver.go')
+    stdin.flush()
+    # if stdout.channel.recv_exit_status():
+        # print(ENTRY_NAME, " failed to start entryserver")
+    time.sleep(2)
+    client.close()
+
+# type is name of process either ['client', 'entry', 'server']
+def killall(dns_name, ptype):
+    if ptype == 'entry':
+        cmd = "killall -2 entryserver"
+    elif ptype == 'server':
+        cmd = "killall -2 main"
+    else:
+        cmd = "killall -2 Python"
     
-    # for instance in running_instances:
-    #     client = paramiko.SSHClient()
-    #     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    print("Killing ", ptype, " on ", dns_name)
 
-    #     client.connect(hostname=instance.public_dns_name, username="ubuntu", pkey=key)
-    #     stdin, stdout, stderr = client.exec_command('git clone https://github.com/edwjchen/agario.git')
-    #     stdin.flush()
-    #     if stdout.channel.recv_exit_status():
-    #         print(instance.public_ip_address, " failed clone")
+    key = paramiko.RSAKey.from_private_key_file("the-key-to-her-heart.pem")
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-    #     stdin, stdout, stderr = client.exec_command('cd agario/src/setup')
-    #     stdin.flush()
-    #     if stdout.channel.recv_exit_status():
-    #         print(instance.public_ip_address, " failed cd?")
+    client.connect(hostname=dns_name, username="ubuntu", pkey=key)
+    stdin, stdout, stderr = client.exec_command(cmd)
+    stdin.flush()
+    time.sleep(2)
+ 
+    if stdout.channel.recv_exit_status():
+        print(dns_name, "failed to kill")
+    else:
+        print("Killed", ptype, "successfully")
+    client.close()
 
-    #     stdin, stdout, stderr = client.exec_command('. setup.sh')
-    #     stdin.flush()
-    #     if stdout.channel.recv_exit_status():
-    #         print(instance.public_ip_address, " failed setup?")
-        
-    pass
-    
+def get_logs(dns_name):
+    key = paramiko.RSAKey.from_private_key_file("the-key-to-her-heart.pem")
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    client.connect(hostname=dns_name, username="ubuntu", pkey=key)
+    stdin, stdout, stderr = client.exec_command('scp agario/src/peer_to_peer/client/logs/* ../data/experiment_{};'.format(EXPERIMENT_NAME))
+    stdin.flush()
+    time.sleep(5)
+    if stdout.channel.recv_exit_status():
+        print(dns_name, " failed to scp")
+    client.close()
+
 # instance_ids = []
 
 # ec2_client = boto3.client('ec2')
@@ -174,33 +234,96 @@ def setup_instances(num):
 #     print()
 
 if __name__ == '__main__':
-    args = parser.parse_args()
-    if bool(args.stats):
-        get_stats()
+    if (len(sys.argv) == 1):
+        print("Missing experiment name!")
+        sys.exit(0)
 
-    if bool(args.hostname):
-        get_hostnames()
+    EXPERIMENT_NAME = sys.argv[1]
+    if not os.path.exists('../data/'+EXPERIMENT_NAME):
+        os.mkdir('../data/'+EXPERIMENT_NAME)
 
-    if bool(args.verify):
-        verify_instance_setup()
+    print("Welcome to the agar.io experimental CLI.")
+    print("You are running experiment " + EXPERIMENT_NAME)
+    last_cmd = ""
+    while True:
+        cmds = input('> ').split(" ")
+        cmd_type = cmds[0]
 
-    if int(args.create):
-        create_instances(int(args.create))
+        # press enter to redo
+        if cmd_type == "" and last_cmd != "":
+            cmd_type = last_cmd
+        else:
+            last_cmd = cmd_type
 
-    if int(args.start):
-        start_instances(int(args.start))
+        if cmd_type == "help":
+            print("Not again... oh well here you go\n")
+            print("stats hostnames verify create start stop")
+            print("terminate setup wait start_entry kill_entry")
+            print("start_servers start_clients kill_server_client")
+            print("data refresh")
+        elif cmd_type == "stats":
+            get_stats()
+        elif cmd_type == "hostnames":
+            get_hostnames()
+        elif cmd_type == "verify":
+            verify_instance_setup()
+        elif cmd_type == "create":
+            create_instances(int(cmds[1]))
+        elif cmd_type == "start":
+            start_instances(int(cmds[1]))
+        elif cmd_type == "stop":
+            stop_instances(int(cmds[1]))
+        elif cmd_type == "terminate":
+            terminate_instances(int(cmds[1]))
+        elif cmd_type == "setup":
+            setup_instances(int(cmds[1]))
+        elif cmd_type == "wait":
+            time.sleep(int(cmds[1]))
+        elif cmd_type == "refresh":
+            refresh_instances()
+        elif cmd_type == "start_entry":
+            start_entry()
+        elif cmd_type == "kill_entry":
+            print(ENTRY_NAME)
+            killall(ENTRY_NAME, 'entry')
+        elif cmd_type == "start_servers": # starts some number of player servers
+            num_to_start = int(cmds[1])
+            pass
+        elif cmd_type == "start_clients": # starts clients on some number of servers that are already running player servers
+            pass
+        elif cmd_type == "kill_server_client": # kills some number of player servers along with their clients
+            num_to_kill = int(cmds[1])
+            pass
+        elif cmd_type == "data": # run scp on all servers that are running
+            pass
+        elif cmd_type in ['quit', 'q']:
+            sys.exit(0)
+        else:
+            print("GIT GUD")
+        # elif cmd_type == "start_entry":
 
-    if int(args.stop):
-        stop_instances(int(args.stop))
+# if __name__ == '__main__':
+#     args = parser.parse_args()
+#     if bool(args.stats):
+#         get_stats()
 
-    if int(args.terminate):
-        terminate_instances(int(args.terminate))
+#     if bool(args.hostname):
+#         get_hostnames()
 
-    if int(args.setup):
-        setup_instances(int(args.setup))
+#     if bool(args.verify):
+#         verify_instance_setup()
 
+#     if int(args.create):
+#         create_instances(int(args.create))
 
-    
+#     if int(args.start):
+#         start_instances(int(args.start))
 
+#     if int(args.stop):
+#         stop_instances(int(args.stop))
 
+#     if int(args.terminate):
+#         terminate_instances(int(args.terminate))
 
+#     if int(args.setup):
+#         setup_instances(int(args.setup))
